2492370d500b625f7e447aa6f98e62c5b2fb8eaa
Karl Romet Somelar <karlromets@gmail.com>
2024-12-28 21:57:50 +0200

    Update imports and add types/ws

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -2,11 +2,11 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import format from "./i18n-format.js";
-import translationEN from "public/locales/en/translation.json";
-import translationES from "public/locales/es/translation.json";
-import translationID from "public/locales/id/translation.json";
-import translationET from "public/locales/et/translation.json";
-import translationFR from "public/locales/fr/translation.json";
+import translationEN from "../public/locales/en/translation.json";
+import translationES from "../public/locales/es/translation.json";
+import translationID from "../public/locales/id/translation.json";
+import translationET from "../public/locales/et/translation.json";
+import translationFR from "../public/locales/fr/translation.json";
 
 const resources = {
     en: {
