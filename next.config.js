c1c3c0c6f5b8c962ea6889f11d908e7b5e655cb6
Karl Romet Somelar <karlromets@gmail.com>
2024-12-28 19:21:48 +0200

    Setup typescript configuration

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ module.exports = {
   experimental: { appDir: true },
   webpack(config) {
     config.experiments = { ...config.experiments, topLevelAwait: true };
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': '.',
+      '@components': './components',
+      '@i18n': './i18n'
+    };
     return config;
   },
   compiler: {
